20590b4ae934149d7a0141211ec74230ddf04fe2
Roi Daradal <roidaradal@gmail.com>
2025-04-14 21:21:56 +0800

    v0.4.2 - Day 04.2

--- day04.py
+++ day04.py
@@ -2,6 +2,7 @@
 # John Roy Daradal 
 
 # SolutionA: 26443
+# SolutionB: 6284877
 
 from utils import * 
 
@@ -23,6 +24,11 @@ def day04A():
         total += computeScore(card)
     print(total)
 
+def day04B():
+    full = True 
+    total = countTotalCards(input04(full))
+    print(total)
+
 def getNumbers(line: str) -> set[int]:
     return set(int(x.strip()) for x in line.strip().split())
 
@@ -33,5 +39,18 @@ def computeScore(card: game) -> int:
         return 0
     return 2 ** (common-1)
 
+def countTotalCards(cards: list[game]) -> int:
+    limit = len(cards)
+    count = {i:1 for i in range(limit)}
+    for i,card in enumerate(cards):
+        winners, numbers = card 
+        common = len(winners.intersection(numbers))
+        for j in range(common):
+            k = i + j + 1 
+            if k < limit:
+                count[k] += count[i]
+    return sum(count.values())
+
 if __name__ == '__main__':
-    day04A()
+    # day04A()
+    day04B()
